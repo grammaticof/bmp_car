6f2f2c550304b3d0e7708fb75161a7f7d02aa95e
Francesco Grammatico <grammatico.francesco@gmail.com>
2016-05-21 18:09:22 +0100

    refactor bitmap_editor_spec

--- spec/lib/bitmap_editor_spec.rb
+++ spec/lib/bitmap_editor_spec.rb
@@ -6,7 +6,6 @@ describe BitmapEditor do
   context 'STDIN sequence I 5 6, L 2 3' do
     it 'should return the expected value' do
       allow(subject).to receive(:gets).and_return('I 5 6', 'L 2 3 C', 'S', 'X')
-      allow(subject).to receive(:loop).and_yield
 
       expect { subject.run }
         .to output(/OOOOO\nOOOOO\nOCOOO\nOOOOO\nOOOOO\nOOOOO/)
@@ -17,7 +16,6 @@ describe BitmapEditor do
   context 'STDIN sequence I 5 6, L 2 3, V 2 3 6 W, H 3 5 2 Z' do
     it 'should return the expected value' do
       allow(subject).to receive(:gets).and_return('I 5 6', 'L 2 3 C', 'V 2 3 6 W', 'H 3 5 2 Z', 'S', 'X')
-      allow(subject).to receive(:loop).and_yield
 
       expect { subject.run }
         .to output(/OOOOO\nOOZZZ\nOWOOO\nOWOOO\nOWOOO\nOWOOO/)
